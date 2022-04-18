59a262204fbb93bb89ed6d1833b8f87f03eac471
VictorKostiuk <vityok.kostiuk@gmail.com>
2022-04-18 22:32:05 +0300

    Changed migrations

--- db/migrate/20220315125112_create_checks.rb
+++ db/migrate/20220315125112_create_checks.rb
@@ -3,6 +3,7 @@
 class CreateChecks < ActiveRecord::Migration[7.0]
   def change
     create_table :checks do |t|
+      t.belongs_to :total_sum, foreign_key: true
       t.string :title
       t.text :body
       t.integer :cost
