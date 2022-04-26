76ae84d99ebb91546e0585acbed3731376198616
VictorKostiuk <vityok.kostiuk@gmail.com>
2022-04-26 15:02:01 +0300

    Case fixed

--- spec/features/full_flow_spec.rb
+++ spec/features/full_flow_spec.rb
@@ -2,14 +2,7 @@
 
 RSpec.describe 'Full flow', driver: :selenium_chrome do
 
-  it 'Successfully sign up' do
-    sign_up
-
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
-    expect(page).to have_current_path root_path, ignore_query: true
-  end
-
-  it 'Registered user to sign in' do
+  it 'creates everything' do
     email = Faker::Internet.unique.email
     visit new_user_registration_path
     fill_in 'Email', with: email
@@ -17,7 +10,12 @@ RSpec.describe 'Full flow', driver: :selenium_chrome do
     fill_in 'Password confirmation', with: '12345678'
     click_on 'Sign up'
 
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(page).to have_current_path root_path, ignore_query: true
+
     click_on 'Sign out'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
 
     visit new_user_session_path
     fill_in 'Email', with: email
@@ -26,56 +24,24 @@ RSpec.describe 'Full flow', driver: :selenium_chrome do
 
     expect(page).to have_content 'Signed in successfully.'
     expect(page).to have_current_path root_path, ignore_query: true
-  end
 
-  it 'User signed out' do
-    sign_up
-
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
-    expect(page).to have_current_path root_path, ignore_query: true
-
-    click_on 'Sign out'
-
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
-  end
-
-  it 'Registered user try create account' do
-    sign_up
     create_account
 
     expect(page).to have_content 'You are being redirected.'
-  end
 
-  it 'Registered user creates category' do
-    sign_up
     visit categories_path
     click_on 'New category'
     create_category
     visit categories_path
 
     expect(page).to have_content 'test1'
-  end
 
-  it 'Registered user creates type' do
-    sign_up
     visit new_type_path
     create_type
 
     expect(page).to have_content 'You are being redirected.'
     expect change(Type, :count).by(+1)
-  end
 
-  it 'Registered user try create transaction' do
-    sign_up
-    visit accounts_path
-    create_account
-    click_on 'redirected'
-    click_on 'test', match: :first
-    click_on 'New Category'
-    create_category
-    click_on 'redirected'
-    visit new_type_path
-    create_type
     visit accounts_path
     click_on 'test', match: :first
     create_transaction
